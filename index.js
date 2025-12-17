b7ca564221d360d21dad3018b6469d10aa69ca01
peiallll <teklik300@gmail.com>
2025-12-17 21:16:00 +0000

    initial commit
    search for elements, work out relative atomic mass for an element by number of molecules (only elements 1-20)

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium"]
+let resultEl = document.getElementById("result-el")
+let errorEl = document.getElementById("error-el")
+
+const atomicMasses = {
+        Hydrogen: 1,
+        Helium: 4,
+        Lithium: 7,
+        Beryllium: 9,
+        Boron: 11,
+        Carbon: 12,
+        Nitrogen: 14,
+        Oxygen: 16,
+        Fluorine: 19,
+        Neon: 20,
+        Sodium: 23,
+        Magnesium: 24,
+        Aluminium: 27,
+        Silicon: 28,
+        Phosphorus: 31,
+        Sulfur: 32,
+        Chlorine: 35.5,
+        Argon: 40,
+        Potassium: 39,
+        Calcium: 45
+    }
+
+function getRelativeAtomicMass() {
+    const elementInputEl = document.getElementById("element-input");
+    const userElementValue = elementInputEl.value;
+
+    const multiplierInputEl = document.getElementById("number-of-element-input");
+    const userMultiplierEl = multiplierInputEl.value;
+    
+    const withMolecules = Number(atomicMasses[userElementValue] * userMultiplierEl);
+
+    if (userElementValue in atomicMasses) {
+        errorEl.textContent = "";
+        resultEl.textContent = `Relative atomic mass: ${withMolecules}`;
+    } else {
+        resultEl.textContent = "";
+        errorEl.textContent = `Element "${userElementValue}" not found`;
+    }
+}
