25a16f9f4f20652d70ca169a8aa055afe5665f34
Max Preston <teklik300@gmail.com>
2025-12-21 13:41:01 +0000

    main styling, added history, font, info and code polish

--- rfm.js
+++ rfm.js
@@ -1,5 +1,6 @@
 let resultEl = document.getElementById("result-el")
 let errorEl = document.getElementById("error-el")
+let previousEl = document.getElementById("previous-el")
 // variables
 const atomicMasses = { // atomic masses list
         Hydrogen: 1,
@@ -21,7 +22,37 @@ const atomicMasses = { // atomic masses list
         Chlorine: 35.5,
         Argon: 40,
         Potassium: 39,
-        Calcium: 40
+        Calcium: 40,
+        Scandium: 45,
+        Titanium: 48,
+        Vanadium: 51,
+        Chromium: 52,
+        Manganese: 55,
+        Iron: 56,
+        Cobalt: 59,
+        Nickel: 59,
+        Copper: 64,
+        Zinc: 65,
+        Gallium: 70,
+        Germanium: 73,
+        Arsenic: 75,
+        Selenium: 79,
+        Bromine: 80,
+        Krypton: 84,
+        Rubidium: 85,
+        Strontium: 88,
+        Yttrium: 89,
+        Zirconium: 91,
+        Niobium: 93,
+        Molybdenum: 96,
+        Technetium: 98,
+        Ruthenium: 101,
+        Rhodium: 103,
+        Palladium: 106,
+        Silver: 108,
+        Cadmium: 112,
+        Indium: 115,
+        Tin: 119
     }
 
 function getRelativeFormulaMass() {
@@ -33,18 +64,18 @@ function getRelativeFormulaMass() {
     
     if (!userMultiplierEl || userMultiplierEl <= 0) {
     errorEl.textContent = "Please enter a valid number of molecules";
-    resultEl.textContent = "";
+    resultEl.textContent = "Result: ";
     return;
 }
 // check user validity 
 
     if (userElementValue in atomicMasses) { // check user validity
-
         const formulaMass = Number(atomicMasses[userElementValue] * userMultiplierEl); // atomic mass * number of molecules
         errorEl.textContent = "";
         resultEl.textContent = `Relative formula mass: ~${formulaMass}`;
+        previousEl.textContent += formulaMass + " | "
     } else {
-        resultEl.textContent = "";
+        resultEl.textContent = "Result: ";
         errorEl.textContent = `Element "${userElementValue}" not found (*please enter initial letter of element as Capital (f.e 'Boron', not 'boron'))`; // error message
     }
 }
--- rim.js
+++ rim.js
@@ -1,6 +1,10 @@
 let errorEl = document.getElementById("error-el")
 let resultEl = document.getElementById("result-el")
 
+let arrValues = [];
+
+let previousEl = document.getElementById("previous-el")
+
 function isBlank() {
     for (let i = 0; i < arrValues.length; i++) {
         if (!arrValues[i].toString().trim()) {
@@ -29,23 +33,21 @@ function getRelativeIsotopicMass() {
     const isotope2AbundancyEl = document.getElementById("input-abundancy2-el")
     const isotope2Abundancy = Number(isotope2AbundancyEl.value);
 
-    
+    arrValues = [userElement, isotope1Mass, isotope1Abundancy, isotope2Mass, isotope2Abundancy]
+
     if (isBlank()) {
         resultEl.textContent = ""
         errorEl.textContent = "You left a field blank."
         return;
     }
 
-    arrValues = [userElement, isotope1Mass, isotope1Abundancy, isotope2Mass, isotope2Abundancy]
-    
     if (Number(isotope2Abundancy) + Number(isotope1Abundancy) != 100) {
-        resultEl.textContent = ""
+        resultEl.textContent = "Result: "
         errorEl.textContent = "Abundancies don't add up to 100%"
         return;
     }
     let numerator = (isotope1Mass * isotope1Abundancy) + (isotope2Mass * isotope2Abundancy)
     let total = numerator / 100
     resultEl.textContent = `Result: ${total}`
+    previousEl.textContent += total + " | "
 }
-
-let arrValues = [];
